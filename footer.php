bae8e0a7a48579e820fd687a9ad267917ba85664
Theodore Marshall <ted@tedm.net>
2019-12-28 13:25:38 -0800

    -Increase footer font sizes for contact, navigation, and social media links

--- footer.php
+++ footer.php
@@ -2,6 +2,7 @@
 <div class="footer-wrap">
 
 	<footer class="site-footer">
+		
 		<section class="footer-company-info" itemscope itemtype="https://schema.org/ProfessionalService">
 			<h4 class="name" itemprop="name">Ted Marshall</h4>
 			<ul>
@@ -9,6 +10,7 @@
 				<li class="ftr-contact" itemprop="telephone"><a href="tel:5037845673" class="lbp_secondary"><span class="email-icon" aria-hidden="true" data-icon="&#xe005;"></span> (503) 784-5673</a></li>
 			</ul>
 		</section><!-- .contact-ftr -->
+		
 		<section class="footer-navigation">
 			<ul>
 				<li><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><span class="nav-icon" aria-hidden="true" data-icon="&#xe001;"></span> <span class="nav-text">Portfolio</span></a></li>
@@ -17,11 +19,14 @@
 				<li><a href="<?php echo esc_url( home_url( '/contact/' ) ); ?>"><span class="nav-icon" aria-hidden="true" data-icon="&#xe003;"></span> <span class="nav-text">Contact</span></a></li>
 			</ul>
 		</section><!-- .nav-ftr -->
+		
 		<section class="footer-social-links">
 			<h4>Follow Me on ...</h4>
 			<?php get_template_part('inc/inc-socialmedia'); ?>
 		</section><!-- .social-ftr -->
+		
 		<p class="copyright">&copy; <? echo date("Y"); ?> Theodore Marshall. All Rights Reserved.</p>
+		
 	</footer>
 	
 </div><!-- .footer-wrap -->
